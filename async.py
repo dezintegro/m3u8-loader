0b6d47d88723855781e9f1bac6f6705eb96862c7
dezintegro <dezintegro@gmail.com>
2017-07-21 22:29:11 +0300

    Added sync loader in loader.py Added async loader witn aiohttp in async.py Added utils for async downloading

--- async.py
+++ async.py
@@ -1,23 +1,28 @@
 import shutil
 import m3u8
+
 from async_download import *
 
 
 MAX_CONNECTIONS = 100
-resolution_index = 1
+resolution_index = -1
+m3u8_file = 'vk.m3u8'
+# m3u8_file = 'master.m3u8'
 infile = 'merged.ts'
 outfile = 'merged.mp4'
 
-m3u8_obj = m3u8.load('master.m3u8')  # this could also be an absolute filename
-playlists = m3u8_obj.playlists
+m3u8_obj = m3u8.load(m3u8_file)  # this could also be an absolute filename
+if not m3u8_obj.is_endlist:
+    playlists = m3u8_obj.playlists
 
-print('Доступные разрешения:')
-for playlist in playlists:
-    print(playlist.stream_info.resolution)
+    print('Доступные разрешения:')
+    for playlist in playlists:
+        print(playlist.stream_info.resolution)
 
-# TODO выбор разрешения
+    # TODO выбор разрешения
+    m3u8_obj = m3u8.load(playlists[resolution_index].absolute_uri)
 
-m3u8_obj = m3u8.load(playlists[resolution_index].absolute_uri)
+
 segments_count = len(m3u8_obj.segments)
 print('Всего {} частей'.format(segments_count))
 urls = [segment.absolute_uri for segment in m3u8_obj.segments]  # [:10]
@@ -39,13 +44,4 @@ for file in files:
     with open(file, 'rb') as in_file, open('merged.ts', 'ab') as out_file:
         shutil.copyfileobj(in_file, out_file)
 
-#
-# for i, segment in enumerate(m3u8_obj.segments):
-#     print('Загрузка части {} из {}'.format(i, segments_count))
-#     with urllib.request.urlopen(segment.absolute_uri) as response, open('merged.ts', 'ab') as out_file:
-#         shutil.copyfileobj(response, out_file)
-
-# print('Конвертируем видео')
-
-# subprocess.run(['ffmpeg', '-i', infile, outfile])
 print('Готово!')
